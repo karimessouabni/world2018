c392752a709e5c61a13f8f19f3c81d9e2884c039
karim Essouabni <karim@karim.local>
2018-01-16 19:59:00 +0100

    [BG01] Correction erreur d'affichage pour IOS

--- src/pages/item-detail/item-detail.ts
+++ src/pages/item-detail/item-detail.ts
@@ -26,13 +26,13 @@ import { trigger, style, transition, animate, group }from '@angular/animations';
   animations: [
     trigger('itemAnim', [
       transition(':enter', [
-        style({transform: 'translateX(-100%)'}),
-        animate(350)
+        style({transform: 'translateX(-50%)'}),
+        animate(150)
       ]),
       transition(':leave', [
         group([
           animate('0.2s ease', style({
-            transform: 'translate(150px,25px)'
+            transform: 'translate(150px,55px)'
           })),
           animate('0.5s 0.2s ease', style({
             opacity: 0
