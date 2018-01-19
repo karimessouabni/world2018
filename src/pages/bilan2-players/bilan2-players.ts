ab22f90f6e7c8708d4bdcb5be767694e0c2206e8
karim Essouabni <karim@karim.local>
2018-01-19 19:35:19 +0100

    Revert "Google cloud function Test"
    This reverts commit 044725fae99bc06f03551d2136a5e0f03a8df738.

--- src/pages/bilan2-players/bilan2-players.ts
+++ src/pages/bilan2-players/bilan2-players.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { WelcomePage} from '../pages'
 
-import { Player } from '../../models/Models';
-
-import { PlayerProvider } from '../../providers/providers';
 /**
  * Generated class for the Bilan2PlayersPage page.
  *
@@ -19,18 +15,9 @@ import { PlayerProvider } from '../../providers/providers';
 })
 export class Bilan2PlayersPage {
   fixture: any;
-  player: Player = new Player();
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public playerProvider: PlayerProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.fixture = navParams.get('fixture');
-    playerProvider.getAuthPlayerInfs().subscribe(user => {
-      if (user) {
-        this.player = user[0];
-      } else {
-        // if the user is unloged redirection to welcom page 
-        this.navCtrl.push(WelcomePage);
-      }
-    });
   }
 
   ionViewDidLoad() {
